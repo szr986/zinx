d6e3a4bdaa0268a1f9063372f88c3c39eb4a969d
szr986 <44153262@qq.com>
2022-09-16 16:25:28 +0800

    zinxV0.2

---
+++ ziface/iconnection.go
@@ -0,0 +1,21 @@
+package ziface
+
+import "net"
+
+type IConnection interface {
+	// 启动连接，让当前连接准备开始工作
+	Start()
+	// 停止连接，结束当前连接工作
+	Stop()
+	// 获取当前连接绑定的socket conn
+	GetIPConnection() *net.TCPConn
+	// 获取当前连接模块的连接id
+	GetConnID() uint32
+	// 获取远程客户端的tcp状态 IP port
+	RemoteAddr() net.Addr
+	// 发送数据,将数据发送给客户端
+	Send(data []byte) error
+}
+
+// 定义一个处理连接义务的方法(连接，数据内容，数据长度)
+type HandleFunc func(*net.TCPConn, []byte, int) error
